f2ccfe3811497397bc402cc5f1cc4735b03da0d7
Lincoln Harris <lharris1@hchb.com>
2022-03-23 08:28:04 -0500

    something

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
 
   getAlbum(id : number) : Observable<Album> {
     return this._http.get(this._albumUrl).pipe(
-      map((response) => <Album>response.json())
+      map((response) => <Album>response.json().album)
     );
   }
 
